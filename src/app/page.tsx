fbb9e93821adb5d1c90780146eedbe407e12f6ad
Jonas Nygaard <jonasnygaard96@gmail.com>
2025-02-21 12:53:37 +0100

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,12 +13,13 @@ import {
 export default function Home() {
   return (
     <>
-      <div className="xl:bg-[url('/painting2.jpg')] lg:bg-[url('/painting3.jpg')] md:bg-[url('/painting5.jpg')] bg-[url('/painting8.jpg')]  relative  h-screen bg-cover">
+      <div className="xl:bg-[url('/painting9.jpg')] lg:bg-[url('/painting10.jpg')] md:bg-[url('/painting12.jpg')]  bg-[url('/painting8.jpg')]  relative  h-screen bg-cover">
         <div className="w-full h-full flex justify-center md:justify-start md:items-center lg:pl-20 md:pl-10">
-          <div className="lg:text-6xl text-4xl mt-20 sm:text-6xl font-bold md:text-4xl">
+          <div className="lg:text-5xl text-4xl mt-20 sm:text-6xl  font-bold md:text-4xl">
             <h1>Get your image</h1>
-            <h1 className="mb-1">
-              on a <span>custom</span> canvas
+            <h1 className="mb-1 md:mb-0">
+              on a <span>custom</span>{" "}
+              <span className="md:block lg:inline-block">canvas</span>
             </h1>
             <Link
               className="bg-stone-900 text-white text-lg md:text-lg lg:text-xl py-3 px-5 rounded-xl"
