dfcd5d547e36a886fad670483f1cbdb4c09de98d
Jonas Nygaard <jonasnygaard96@gmail.com>
2025-02-21 12:24:12 +0100

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,15 +13,15 @@ import {
 export default function Home() {
   return (
     <>
-      <div className="bg-[url('/painting2.jpg')] relative  h-screen bg-cover">
-        <div className="w-full h-full flex justify-start items-center md:pl-40">
-          <div className="text-6xl font-semibold">
+      <div className="xl:bg-[url('/painting2.jpg')] lg:bg-[url('/painting3.jpg')] md:bg-[url('/painting5.jpg')] bg-[url('/painting8.jpg')]  relative  h-screen bg-cover">
+        <div className="w-full h-full flex justify-center md:justify-start md:items-center lg:pl-20 md:pl-10">
+          <div className="lg:text-6xl text-4xl mt-20 sm:text-6xl font-bold md:text-4xl">
             <h1>Get your image</h1>
-            <h1>
+            <h1 className="mb-1">
               on a <span>custom</span> canvas
             </h1>
             <Link
-              className="bg-stone-900 text-white text-xl py-3 px-5 rounded-xl"
+              className="bg-stone-900 text-white text-lg md:text-lg lg:text-xl py-3 px-5 rounded-xl"
               href={"/configure/upload"}
             >
               Upload image
